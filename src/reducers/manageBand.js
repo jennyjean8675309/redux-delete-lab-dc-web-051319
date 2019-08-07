f41cbac0c85548ce0917960925d8dcb689949fec
Jennifer Ingram <jennifer.ingram@flatironschool.com>
2019-08-07 15:46:55 -0400

    added the functionality to delete a band

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -3,9 +3,15 @@ export default function manageBand(state = {
 }, action) {
   switch (action.type) {
     case 'ADD_BAND':
-
-      return { ...state, bands: [...state.bands, action.name] }
-
+      const band = {
+        id: Math.random()*10000000000000000,
+        name: action.name
+      }
+      return { ...state, bands: [...state.bands, band] }
+    case 'DELETE_BAND':
+      console.log('deleting this band', action.id)
+      let filteredBands = state.bands.filter( band => band.id !== action.id)
+      return {...state, bands: filteredBands}
     default:
       return state;
   }
